3ecebdff29c7bf6aa0c8c40cb9ba408ab10d1323
EugeneShatalin <shatalin_ei@mail.ru>
2020-04-11 01:35:11 +0600

    Lesson 35. Handling Events

--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -2,17 +2,25 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+
+        onLabelClick = () => {
+            console.log(this.props.label)
+        }
+
     render() {
         const {label, important = false} = this.props;
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
-
-
         return (
             <span className="todo-list-item">
-                <span className="todo-list-item-label" style={style}>{label}</span>
+                <span className="todo-list-item-label"
+                      style={style}
+                      onClick={ this.onLabelClick }
+                >
+                    {label}
+                </span>
                 <button type="button"
                         className="btn btn-outline-success btn-sm float-right">
                     <i className="fa fa-exclamation"/>
