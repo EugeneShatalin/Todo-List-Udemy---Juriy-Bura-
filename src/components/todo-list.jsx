758251c130acbc7ab62c841fae7c5e898ec69068
EugeneShatalin <shatalin_ei@mail.ru>
2020-04-10 18:13:53 +0600

    Lesson 30. Arrays as properties components

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -5,9 +5,11 @@ import TodoListItem from './todo-list-item';
 const TodoList = ({todos}) => {
 
     const elements = todos.map((item) => {
+
+        const {id, ...itemProps} = item;
         return (
-            <li>
-                <TodoListItem {...item} />  {/*заменили label={elem.label} important={elem.important}
+            <li key={item.id}>
+                <TodoListItem {...itemProps} />  {/*заменили label={elem.label} important={elem.important}
                                                 на {...item} */}
             </li>
         );
